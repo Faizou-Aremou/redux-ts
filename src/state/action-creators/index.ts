97d1f75887721ce72637e75b4b01918073f8d9ff
Faizou <faizouaremou@gmail.com>
2023-10-16 23:39:02 +0200

    action creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,12 +1,27 @@
+import axios from 'axios';
 import { ActionType } from '../action-type';
+import { Dispatch } from 'redux';
+import { Action } from '../action';
 
-const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+export const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES,
     });
 
     try {
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        { params: { text: term } }
+      );
+
+      const names: string[] = data.objects.map(
+        (result: any) => result.package.name
+      );
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
     } catch (err) {
       if (err instanceof Error) {
         dispatch({
