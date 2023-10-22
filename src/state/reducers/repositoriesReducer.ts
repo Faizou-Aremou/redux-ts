30f7540a71c0ccd283c2048d8dffc5ffd95e2130
Faizou <faizouaremou@gmail.com>
2023-10-22 04:00:26 +0200

    finish react redux

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -7,17 +7,22 @@ interface RepositoriesState {
   error: string | null;
 }
 
+const initialState: RepositoriesState = {
+  data: [],
+  loading: false,
+  error: null,
+};
 const reducer = (
-  state: RepositoriesState,
+  state: RepositoriesState= initialState,
   action: Action
 ): RepositoriesState => {
   switch (action.type) {
     case ActionType.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: [] };
     case ActionType.SEARCH_REPOSITORIES_SUCCESS:
-      return { loading: true, error: null, data: action.payload as string[] };
+      return { loading: false, error: null, data: action.payload as string[] };
     case ActionType.SEARCH_REPOSITORIES_ERROR:
-      return { loading: true, error: action.payload as string, data: [] };
+      return { loading: false, error: action.payload as string, data: [] };
     default:
       return state;
   }
